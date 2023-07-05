6f50e73f31058c02b29f5f35b87e0794861f0f2e
Metatronx64 <osagieevba@gmail.com>
2023-07-05 04:22:06 -0700

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+
+def match_school(argument)
+  regex = /School/
+  if argument.match?(regex)
+    puts "Match found!"
+  else
+    puts "No match found."
+  end
+end
+
+# Accept one argument from the command line
+argument = ARGV[0]
+
+# Call the matching method
+match_school(argument)
