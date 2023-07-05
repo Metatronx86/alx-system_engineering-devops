fd5f295bd727fb833e34439eadc6c0857f381751
Metatronx64 <osagieevba@gmail.com>
2023-07-05 04:29:07 -0700

    fixe code logic

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,16 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_school(argument)
-  regex = /School/
-  if argument.match?(regex)
-    puts "Match found!"
-  else
-    puts "No match found."
-  end
-end
-
-# Accept one argument from the command line
-argument = ARGV[0]
-
-# Call the matching method
-match_school(argument)
+puts ARGV[0].scan(/School/).join
